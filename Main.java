ed485744adf6b33192c5a7cf9c85bcc05b2ba549
Oleg <oleg@localhost>
2021-03-10 21:42:49 +0300

    First

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static int bonus (int price){
+        int bonusUnit = 20;
+        int bonusMiles = price / bonusUnit;
+        return bonusMiles;
+    }
+    public static void main (String[] args){
+        System.out.println("Поздравляем! Вам начисленно " + bonus(17365) + " бонусных миль");
+    }
+}
+
