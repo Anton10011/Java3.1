85b0942371aebd08b994450863845c015b5a2f8e
Anton10011 <78318766+Anton10011@users.noreply.github.com>
2021-03-14 17:29:08 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,10 @@
 public class Main {
-    public static int bonus (int price){
+    public static int bonus(int price) {
         int BonusUnit = 20;
         int BonusMiles = price / BonusUnit;
         return BonusMiles;
     }
-    public static void main (String[] args){
+    public static void main(String[] args) {
         System.out.println("Поздравляем! Вам начисленно " + bonus(17365) + " бонусных миль");
     }
 }
-
